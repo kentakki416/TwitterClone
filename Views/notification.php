f3010b466dd1ea7a3df9949c6c215b130fb1b065
LAPTOP-4NKC7PNV\coop <fujimorikenta@icloud.com>
2021-06-07 01:39:01 +0900

    その他のページ

--- Views/notification.php
+++ Views/notification.php
@@ -4,20 +4,19 @@ include_once('../config.php');
 // 便利な関数を読み込む
 include_once('../util.php');
 ?>
-
 <!DOCTYPE html>
 <html lang="ja">
 
 <head>
-<?php include_once('../Views/common/head.php');?>
+  <?php include_once('../Views/common/head.php');?>
   <title>通知画面　/ Twitterクローン</title>
   <meta name="description" content="ホーム画面です" />
 </head>
 
-<body class="home　notification text-center">
+<body class="home notification text-center">
   <div class="container">
-  <!-- サイドメニューの読み込み -->
-  <?php include_once('../Views/common/side.php');?>
+    <!-- サイドメニューの読み込み -->
+    <?php include_once('../Views/common/side.php');?>
     <!-- 投稿画面 -->
     <div class="main">
       <div class="main-header">
@@ -27,26 +26,28 @@ include_once('../util.php');
       <div class="ditch"></div>
 
       <div class="notification-list">
+        <?php if (isset($_GET['case'])):?>
+        <p class="no-result">通知はまだありません。</p>
+        <?php else:?>
         <div class="notification-item">
           <div class="user"><img src="img_uploaded/user/sample-person.jpg" alt=""></div>
-      </div>
-      <div class="content">
-        <p>いいね！されました。</p>
-      </div>
-      </div>
+          <div class="content">
+            <p>いいね！されました。</p>
+          </div>
+        </div>
 
-      <div class="notification-list">
-        <div class="notification-item"><div class="user"><img src="img_uploaded/user/sample-person.jpg" alt=""></div>
-      </div>
-      <div class="content">
-        <p>フォローされました。</p>
-      </div>
-      </div>
-
+        <div class="notification-item">
+          <div class="user"><img src="img_uploaded/user/sample-person.jpg" alt=""></div>
+          <div class="content">
+            <p>フォローされました。</p>
+          </div>
+        </div>
+        <?php endif;?>
       </div>
     </div>
-    <!-- footerのJSの読み込み -->
-<?php include_once('../Views/common/foot.php');?>
+  </div>
+  <!-- footerのJSの読み込み -->
+  <?php include_once('../Views/common/foot.php');?>
 
 
 </body>
